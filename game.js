1fc1913421ad9f2b2c36e7637880aee08ed448aa
jose <joseph0001@gmail.com>
2022-09-01 09:28:00 -0300

    Falta solucionar el problema del player borrando sus pasos anteriores

--- game.js
+++ game.js
@@ -1,20 +1,96 @@
-const canvas = document.querySelector('#game');
-const game = canvas.getContext('2d');
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+let canvasSize;
+let elementsSize;
+const btnup = document.querySelector("#up");
+const btnleft = document.querySelector("#left");
+const btnright = document.querySelector("#right");
+const btndown = document.querySelector("#down");
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
+window.addEventListener("keydown", moveByKeys);
+btnup.addEventListener("click", moveUp);
+btnleft.addEventListener("click", moveLeft);
+btndown.addEventListener("click", moveRight);
+btnright.addEventListener("click", moveDown);
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
-window.addEventListener('load',startGame);
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
 
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
 
+  elementsSize = canvasSize / 10;
+  startGame();
+}
+function render() {
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
+  const mapRowsCols = mapRows.map((row) => row.trim().split(""));
+  mapRowsCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+      if (col == "O") {
+        playerPosition.x = posX;
+        playerPosition.y = posY;
+      }
+
+      game.fillText(emoji, posX, posY);
+    });
+  });
+
+  console.log({ playerPosition });
+}
 function startGame() {
-    // game.fillRect(0,0,100,100);
-    // game.clearRect(0,0,50,50);
-    // game.clearRect()
-    game.font = '25px Verdana';
-    game.fillStyle  = 'purple';
-    game.textAlign = 'center';
-    game.fillText('Jose',25,25)
+  console.log({ canvasSize, elementsSize });
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
+  render();
+
+  movePlayer("PLAYER");
+}
+
+function movePlayer(emo, ) {
+   
+  game.fillText(emojis[emo], playerPosition.x, playerPosition.y);
+}
+
+function moveByKeys(event) {
+  if (event.key == "ArrowUp") moveUp();
+  else if (event.key == "ArrowLeft") moveLeft();
+  else if (event.key == "ArrowRight") moveRight();
+  else if (event.key == "ArrowDown") moveDown();
+}
+function moveUp() {
+    
+
+  
+  const playerPosition2 = playerPosition.y - elementsSize;
 
 
 
 
+  playerPosition.y = playerPosition2;
+
+  movePlayer("PLAYER");
+}
+function moveLeft() {
+  console.log("Izquierda");
+}
+function moveRight() {
+  console.log("Derecha");
+}
+function moveDown() {
+  console.log("Abajo");
 }
